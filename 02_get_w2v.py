1b50de38cd37b28d3c162fa12e0d1739820b3de5
abner <abner1zhou@gmail.com>
2019-12-03 21:11:52 +0800

    修改了worker数量
    Signed-off-by: abner <abner1zhou@gmail.com>

--- 02_get_w2v.py
+++ 02_get_w2v.py
@@ -6,5 +6,5 @@ import gensim
 将第一步创建好的vocab转换成word2vec模型
 主要用到gensim
 """
-model = word2vec.Word2Vec(LineSentence(merger_seg_path), workers=8, min_count=5, size=200)
+model = word2vec.Word2Vec(LineSentence(merger_seg_path), workers=6, min_count=5, size=200)
 model.save("database/wv/word2vec.model")
